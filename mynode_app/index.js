bb7c269deda7caa023dc3a75f7a1be1d6ac0dc69
Nathalia Vieira <naahviera@gmail.com>
2025-02-25 17:20:24 -0300

    add my node app and anotation curso docker

---
+++ mynode_app/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const message = process.env.MESSAGE || "Hello Word!";
+app.get("/",(req,res)=>{
+    res.send(message);
+});
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
